45fab76d86ef3121b2f5aa107be29f69995f143b
zaraqamar261-dotcom <zaraqamar261@gmail.com>
2026-06-15 04:44:36 +0500

    Add application settings designer

---
+++ Properties/Settings.Designer.cs
@@ -0,0 +1,43 @@
+namespace ExamQuestionGenerator.Properties
+{
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+
+        public static Settings Default
+        {
+            get
+            {
+                return defaultInstance;
+            }
+        }
+
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string GeminiApiKey
+        {
+            get
+            {
+                return ((string)(this["GeminiApiKey"]));
+            }
+            set
+            {
+                this["GeminiApiKey"] = value;
+            }
+        }
+
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://generativelanguage.googleapis.com/")]
+        public string GeminiApiEndpoint
+        {
+            get
+            {
+                return ((string)(this["GeminiApiEndpoint"]));
+            }
+        }
+    }
+}
